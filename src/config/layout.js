4d9794038bef317e9131028f17b6eff2dd5618e6
Kyle-Ruan <kyleryk123@gmail.com>
2016-11-12 22:06:44 +1100

    update layout

--- src/config/layout.js
+++ src/config/layout.js
@@ -2,28 +2,28 @@ const layout = {
   columns: [
     {
       rows: [
-        // [
-        //   { name: 'TextInput', span: 12 },
-        //   { name: 'AutoCompleteInput', span: 12 }
-        // ],
-        // [
-        //   { name: 'DateInput', span: 12 },
-        //   { name: 'DateTimeInput', span: 12 }
-        // ],
-        // [
-        //   { name: 'SingleSelect', span: 12 },
-        //   { name: 'MultipleSelect', span: 12 }
-        // ],
-        // [
-        //   { name: 'Boolean', span: 12 },
-        //   { name: 'AsnycMultipleSelect', span: 12 }
-        // ],
-        // [
-        //   { name: 'TextAreaInput', span: 24 }
-        // ],
-        // [
-        //   { name: 'ClientMatching', span: 24 }
-        // ],
+        [
+          { name: 'TextInput', span: 12 },
+          { name: 'AutoCompleteInput', span: 12 }
+        ],
+        [
+          { name: 'DateInput', span: 12 },
+          { name: 'DateTimeInput', span: 12 }
+        ],
+        [
+          { name: 'SingleSelect', span: 12 },
+          { name: 'MultipleSelect', span: 12 }
+        ],
+        [
+          { name: 'Boolean', span: 12 },
+          { name: 'AsnycMultipleSelect', span: 12 }
+        ],
+        [
+          { name: 'TextAreaInput', span: 24 }
+        ],
+        [
+          { name: 'ClientMatching', span: 24 }
+        ],
         [
           { name: 'Clients', span: 24 }
         ]
